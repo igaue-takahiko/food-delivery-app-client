d99b260f3b00b9cce41908d5d0e0d4631d49e537
igaue-takahhiko <hasewo.2610@gmail.com>
2021-03-24 19:55:05 +0900

    add to Header responseve

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
 import {
   makeStyles,
   AppBar,
   Toolbar,
   Typography,
   Button,
+  IconButton,
+  Drawer,
+  CssBaseline
 } from '@material-ui/core';
 
 import { logout } from '../redux/user/actions';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   header: {
     backgroundColor: "#e8ede1",
     marginBottom: 10,
@@ -19,7 +23,10 @@ const useStyles = makeStyles(() => ({
   title: {
     flex: 1,
     marginLeft: "5rem",
-    color: "black"
+    color: "black",
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: "1rem",
+    },
   },
   buttonStyle: {
     margin: "0 8px 0",
@@ -28,12 +35,24 @@ const useStyles = makeStyles(() => ({
   },
   buttons: {
     justifyContent: "space-between",
-    marginRight: "2rem"
+    marginRight: "2rem",
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
   },
-  name: {
-    fontStyle: "bold",
-    fontSize: 32,
+  menuIcon: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
   },
+  drawerPaper: {
+    width: "50%",
+  },
+  drawerButtonStyle: {
+    margin: "16px 32px",
+    color: "black",
+  },
+  toolbar: theme.mixins.toolbar
 }))
 
 const Header = () => {
@@ -46,8 +65,19 @@ const Header = () => {
     address,
   } = useSelector(state => state.user)
 
+  const [ openDrawer, setOpenDrawer ] = useState(false)
+
   const handleLogout = () => {
+    setOpenDrawer(!openDrawer)
     dispatch(logout(history))
+  }
+
+  const handleDrawerToggle = () => {
+    if (!openDrawer) {
+      setOpenDrawer(!openDrawer)
+    } else {
+      setOpenDrawer(false)
+    }
   }
 
   return (
@@ -58,6 +88,91 @@ const Header = () => {
             <span>DeliveryHub</span>
           </Typography>
         </Link>
+        <IconButton
+          className={classes.menuIcon} onClick={handleDrawerToggle}
+          aria-label="Menu Items"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          className={classes.drawerPaper}
+          open={openDrawer} anchor="right"
+          onClose={handleDrawerToggle}
+          variant="temporary"
+          ModalProps={{ keepMounted: true }}
+        >
+          {authenticated ? (
+            role === "ROLE_SELLER" ? (
+              <>
+                <Link to="/seller/dashboard">
+                  <Button
+                    className={classes.drawerButtonStyle} onClick={handleDrawerToggle}
+                  >
+                    Dashboard
+                  </Button>
+                </Link>
+                <Link to="/seller/orders">
+                  <Button
+                    className={classes.drawerButtonStyle} onClick={handleDrawerToggle}
+                  >
+                    Orders
+                  </Button>
+                </Link>
+                <Button
+                  className={classes.drawerButtonStyle}
+                  variant="outlined"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/orders">
+                  <Button
+                    className={classes.drawerButtonStyle} onClick={handleDrawerToggle}
+                  >
+                    Orders
+                  </Button>
+                </Link>
+                <Link to={{ pathname: "/cart", state: { address } }}>
+                  <Button
+                    className={classes.drawerButtonStyle} onClick={handleDrawerToggle}
+                  >
+                    Cart
+                  </Button>
+                </Link>
+                <Button
+                  className={classes.drawerButtonStyle}
+                  variant="outlined"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </Button>
+              </>
+            )
+          ) : (
+            <>
+              <Link to="/login">
+                <Button
+                  className={classes.drawerButtonStyle} onClick={handleDrawerToggle}
+                >
+                  Login
+                </Button>
+              </Link>
+              <Link to="/register">
+                <Button
+                  className={classes.drawerButtonStyle} variant="outlined"
+                  onClick={handleDrawerToggle}
+                >
+                  signup
+                </Button>
+              </Link>
+            </>
+          )}
+        </Drawer>
           {authenticated ? (
             role === "ROLE_SELLER" ? (
               <div className={classes.buttons}>
